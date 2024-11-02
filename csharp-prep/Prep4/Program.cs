65657575f3ff312a4dcd480877c1c5524997b345
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-02 16:00:13 -0300

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
-    }
+        List<int> numList = new List<int>();
+        int num = -1;
+
+        while (num != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            Console.Write("Enter number: ");
+            num = int.Parse(Console.ReadLine());
+            if (num != 0)
+            {
+                numList.Add(num);
+            }
+        }
+
+        int sum = numList.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numList.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numList.Max();
+        Console.WriteLine($"The largest is: {max}");
+
+        numList.Sort();
+        Console.WriteLine("The sorted list is: [" + string.Join(", ", numList) + "]");
+
+       }
 }
