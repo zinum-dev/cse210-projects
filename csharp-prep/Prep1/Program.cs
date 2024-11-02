834cbb752efcb2343e1f3289258dcdaf5811d9bb
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-02 12:11:28 -0300

    core 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,16 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        
         Console.WriteLine("What is your first name?");
         string firstName = Console.ReadLine();
+
         Console.WriteLine("What is your last name?");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
-
-
-
-
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+
+        // Grade Scale:
+        // A >= 90
+        // B >= 80
+        // C >= 70
+        // D >= 60
+        // F < 60
+        if( gradePercentage >= 90) 
+        {
+            Console.WriteLine("A");
+        }
+        else if ( gradePercentage >= 80) 
+        {
+            Console.WriteLine("B");
+        }
+        else if ( gradePercentage >= 70) 
+        {
+            Console.WriteLine("C");
+        }
+        else if ( gradePercentage >= 60) 
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
     }
 }
