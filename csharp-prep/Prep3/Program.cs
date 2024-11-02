f5fefd10eb8606397b1c0442446142a57e1c8920
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-02 14:20:56 -0300

    core1,2,3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+
+
+        int guess = 0;
+        while (magicNumber != guess)
+        {
+
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
