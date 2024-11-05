fd6f322fd7f7aad90325c5c1e7dc1fbbe4707511
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-04 21:11:06 -0300

    prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,55 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+        int square = SquareNumber(num);
+        DisplayResult(name, square);
+
     }
+
+    //DisplayWelcome - Displays the message, "Welcome to the Program!"    
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    /// <summary>
+    /// Asks for and returns the user's name (as a string)
+    /// </summary>
+    /// <returns>The user's name</returns>
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    //PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+
+    //SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+
+
+    //DisplayResult - Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string name, int  number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
+
+
+
 }
