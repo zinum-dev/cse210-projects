0501532dfc328beca488eadf34c89c6c39a9731b
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-05 22:10:30 -0300

    Abstraction

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        
+
+        Resume resume = new();
+        resume._name = "Allison Rose";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
     }
 }
