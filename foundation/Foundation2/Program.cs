496e5c54a2a84559e7409dcf0dd921856eed3283
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-11-19 11:41:43 -0300

    encapsulation foundation

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -13,14 +13,34 @@ class Program
             new Product("pear",0072,1.2,4),
         ];
         Order order1 = new Order(customer1,products1);
-        Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine(order1.GetShippingLabel());
-        Console.WriteLine(order1.GetTotal());
+        DisplayOrder(order1);
         
+        
+
+        Address address2 = new Address("Stanwood Ave", "Jacksonville", "Florida", "USA");
+        Customer customer2 = new Customer("Marco Aurelio",address2);
+        List<Product> products2 =
+        [
+            new Product("rice",0237,4.75,3),
+            new Product("fish",0493,10.02,2),
+            new Product("watermelon",0062,2.6,1),
+        ];
+        Order order2 = new Order(customer2,products2);
+        DisplayOrder(order2);
         
         
 
 
 
+    }
+
+    private static void DisplayOrder(Order order)
+    {
+        Console.Write(new string('-',25));
+        Console.Write(" Order ");
+        Console.WriteLine(new string('-',25));
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine(order.GetTotal());
     }
 }
